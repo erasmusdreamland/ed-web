951eae5e2258058edb2e9a5a30ead08284cc7c53
erasmusdreamland <mgomgar01@gmail.com>
2023-04-29 21:21:27 +0200

    sd

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -5,31 +5,31 @@ import { Link, Route } from "react-router-dom";
 
 export const cardData = [
   {
-    id: 4,
+    id: 5,
     title: 'The Biggest Erasmus Festival | Brno 04.02 ',
     image: '/thumnails-gallery/thumbnail-alvama.jpg',
     linkUrl: "/alvama",
   },
   {
-    id: 3,
+    id: 4,
     title: 'Write On Me Party | Bratislava  30.03 ',
     image: '/thumnails-gallery/thumbnail-draw.jpg',
     linkUrl: "/draw",
   },
   {
-    id: 2,
+    id: 3,
     title: 'Sex Change Party | Bratislava  21.04.2023 ',
     image: '/thumnails-gallery/DSC_2002.jpg',
     linkUrl: "/sex-change",
   },
   {
-    id: 1,
+    id: 2,
     title: 'Football Cup 2nd Edition | Bratislava  22.04.2023 ',
     image: '/thumnails-gallery/DSC_1056 (FILEminimizer).jpg',
-    linkUrl: "/tournament-party",
+    linkUrl: "/tournament",
   },
   {
-    id: 0,
+    id: 1,
     title: 'Football Cup Party & Gala | Bratislava  22.04.2023 ',
     image: '/thumnails-gallery/DSC_1277 (FILEminimizer).jpg',
     linkUrl: "/tournament-party",
@@ -38,7 +38,7 @@ export const cardData = [
     id: 0,
     title: 'Mexican Party | Bratislava  27.04.2023 ',
     image: '/thumnails-gallery/DSC_1445 (FILEminimizer).jpg',
-    linkUrl: "/tournament-party",
+    linkUrl: "/mexican-party",
   },
 
 
