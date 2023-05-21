d688814ddd45a578560b7c9a356cc41e6c17de6c
erasmusdreamland <mgomgar01@gmail.com>
2023-05-21 23:24:19 +0200

    ff

--- src/Components/ArtistSlider.js
+++ src/Components/ArtistSlider.js
@@ -9,22 +9,23 @@ import {
 } from 'react-icons/fa';
 
 
+
 function ArtistSlider({ images }) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [hoveredImageIndex, setHoveredImageIndex] = useState(-1);
   const [visibleImages, setVisibleImages] = useState(4);
   const logoRef = useRef(null);
   const touchStartX = useRef(null);
+  const transitionDuration = 300; // milliseconds
 
-  const handlePrevClick = (event) => {
-    event.preventDefault(); // Prevent default button click behavior
+  const handlePrevClick = () => {
     setCurrentImageIndex(Math.max(0, currentImageIndex - 1));
   };
-  
-  const handleNextClick = (event) => {
-    event.preventDefault(); // Prevent default button click behavior
+
+  const handleNextClick = () => {
     setCurrentImageIndex(Math.min(images.length - visibleImages, currentImageIndex + 1));
   };
+
 
   const startImageIndex = Math.max(0, currentImageIndex);
   const endImageIndex = Math.min(images.length - 1, startImageIndex + visibleImages - 1);
@@ -47,6 +48,8 @@ function ArtistSlider({ images }) {
   };
 
   const handleTouchMove = (event) => {
+    event.preventDefault(); // Prevent default touch event behavior
+
     const touchCurrentX = event.touches[0].clientX;
     const touchDeltaX = touchCurrentX - touchStartX.current;
 
@@ -92,43 +95,56 @@ function ArtistSlider({ images }) {
         onTouchStart={handleTouchStart}
         onTouchMove={handleTouchMove}
       >
-        {visibleImagesArray.map((image, index) => (
-          <div
-            className={`carousel-img-container ${hoveredImageIndex === index ? 'hovered' : ''}`}
-            key={startImageIndex + index}
-            onMouseEnter={() => handleImageHover(index)}
-            onMouseLeave={handleImageLeave}
-          >
-            <div className="carousel-img-details">
-              <img
-                className="carousel-img"
-                src={image.src}
-                alt="carousel"
-                title={image.title}
-                aria-describedby={image.description}
-              />
-              {hoveredImageIndex === index && (
-                <div className="image-details">
-                  <h3>{image.title}</h3>
-                  <p>{image.description}</p>
-                  <div className="artist-icons">
-                    <a href={image.inst}>
-                      &nbsp;
-                      <i className="fa fa-instagram" style={{ color: 'yellow' }}></i>
-                    </a>
-                    <a href={image.tiktok}>
-                      &nbsp;
-                      <i className="fab fa-tiktok" style={{ color: 'yellow' }}></i>
-                    </a>
+        <div
+          className="carousel-slider"
+          style={{
+            transform: `translateX(-${currentImageIndex * (100 / visibleImages)}%)`,
+            transitionDuration: `${transitionDuration}ms`,
+          }}
+        >
+          {visibleImagesArray.map((image, index) => (
+            <div
+              className={`carousel-img-container ${hoveredImageIndex === index ? 'hovered' : ''}`}
+              key={startImageIndex + index}
+              onMouseEnter={() => handleImageHover(index)}
+              onMouseLeave={handleImageLeave}
+            >
+              <div className="carousel-img-details">
+                <img
+                  className="carousel-img"
+                  src={image.src}
+                  alt="carousel"
+                  title={image.title}
+                  aria-describedby={image.description}
+                />
+                {hoveredImageIndex === index && (
+                  <div className="image-details">
+                    <h3>{image.title}</h3>
+                    <p>{image.description}</p>
+                    <div className="artist-icons">
+                      <a href={image.inst}>
+                        &nbsp;
+                        <i className="fa fa-instagram" style={{ color: 'yellow' }}></i>
+                      </a>
+                      <a href={image.tiktok}>
+                        &nbsp;
+                        <i className="fab fa-tiktok" style={{ color: 'yellow' }}></i>
+                      </a>
+                    </div>
                   </div>
-                </div>
+                )}
+              </div>
+              {image.logo && (
+                <img
+                  className="artist-logo"
+                  ref={logoRef}
+                  src={image.logo}
+                  alt={`${image.title} logo`}
+                />
               )}
             </div>
-            {image.logo && (
-              <img className="artist-logo" ref={logoRef} src={image.logo} alt={`${image.title} logo`} />
-            )}
-          </div>
-        ))}
+          ))}
+        </div>
 
         {showLeftArrow && (
           <button className="carousel-arrow prev" onClick={handlePrevClick}>
